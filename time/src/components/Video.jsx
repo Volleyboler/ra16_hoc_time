5b559bba2b2910e356836d9bee62539061e7dbca
Ilia Tregub <forgotten-devil@mail.ru>
2025-02-12 02:43:05 +0500

    update

--- time/src/components/Video.jsx
+++ time/src/components/Video.jsx
@@ -6,7 +6,13 @@ const Pretty = DateTimePretty(DateTime);
 export default function Video(props) {
   return (
     <div className="video">
-      <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <iframe 
+        src={props.url} 
+        title="Видео плеер" // Добавлен уникальный title
+        border="0" // Замена frameBorder на CSS
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
       {/* <DateTime date={props.date} /> */}
       <Pretty date={props.date} />
     </div>
